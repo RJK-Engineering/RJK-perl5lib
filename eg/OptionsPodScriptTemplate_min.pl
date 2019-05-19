4a723dc75b5c3f7926b2e5298229c541d646e7c8
RJK-Engineering <rjklinkhamer@gmail.com>
2019-05-19 19:42:59 +0200

    added examples

---
+++ eg/OptionsPodScriptTemplate_min.pl
@@ -0,0 +1,37 @@
+use strict;
+use warnings;
+
+use Options::Pod;
+
+###############################################################################
+=head1 DESCRIPTION
+
+THIS IS A TEMPLATE.
+
+=head1 SYNOPSIS
+
+script.pl [options] [arguments]
+
+=head1 DISPLAY EXTENDED HELP
+
+script.pl -h
+
+=for options start
+
+=cut
+###############################################################################
+
+Options::Pod::GetOptions(
+    ['HELP'],
+    Options::Pod::HelpOptions("DESCRIPTION|SYNOPSIS|HELP|POD"),
+    ['POD'],
+    Options::Pod::Options
+) || Options::Pod::pod2usage(
+    -sections => "DISPLAY EXTENDED HELP"
+);
+
+@ARGV || Options::Pod::pod2usage(
+    -sections => "DESCRIPTION|SYNOPSIS|DISPLAY EXTENDED HELP"
+);
+
+###############################################################################
---
+++ eg/OptionsPodScriptTemplate_min2.pl
@@ -0,0 +1,43 @@
+use strict;
+use warnings;
+
+use Options::Pod;
+
+###############################################################################
+=head1 DESCRIPTION
+
+THIS IS A TEMPLATE.
+
+=head1 SYNOPSIS
+
+script.pl [options] [arguments]
+
+=head1 DISPLAY EXTENDED HELP
+
+script.pl -h
+
+=for options start
+
+=cut
+###############################################################################
+
+my %opts = ();
+Options::Pod::GetOptions(
+    's|string=s' => \$opts{string}, "Description {argumentname}",
+    'i|integer=i' => \$opts{integer}, "Description {integer}",
+    'e|extendedint=o' => \$opts{extended}, "Description {extendedintegerperlstyle}",
+    'f|realnumber=f' => \$opts{float}, "Description {real}",
+
+    ['HELP'],
+    Options::Pod::HelpOptions("DESCRIPTION|SYNOPSIS|HELP|POD"),
+    ['POD'],
+    Options::Pod::Options
+) || Options::Pod::pod2usage(
+    -sections => "DISPLAY EXTENDED HELP"
+);
+
+@ARGV || Options::Pod::pod2usage(
+    -sections => "DESCRIPTION|SYNOPSIS|DISPLAY EXTENDED HELP"
+);
+
+###############################################################################
