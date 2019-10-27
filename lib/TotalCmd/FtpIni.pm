f63b72bf26923134d55ba4d145b064f92c2bf549
RJK-Engineering <rjklinkhamer@gmail.com>
2019-10-27 16:22:59 +0100

    Added packages

---
+++ lib/TotalCmd/FtpIni.pm
@@ -0,0 +1,97 @@
+=begin TML
+
+---+ package TotalCmd::FtpIni
+
+=cut
+
+package TotalCmd::FtpIni;
+
+use strict;
+use warnings;
+
+use File::Ini;
+
+# see topic "wcx_ftp.ini INI settings" in Total Commander help
+
+###############################################################################
+=pod
+
+---++ Object creation
+
+---+++ new(%attrs) -> TotalCmd::FtpIni
+Returns a new =TotalCmd::FtpIni= object.
+
+=cut
+###############################################################################
+
+sub new {
+    my $self = bless {}, shift;
+    $self->{ini} = new File::Ini(shift);
+    $self->{sessions} = {}; # name => { host, username, anonymous, directory, pasvmode }
+    return $self;
+}
+
+###############################################################################
+=pod
+
+---++ File access
+
+---+++ read([$path]) -> TotalCmd::FtpIni
+Read data from file. Returns false on failure, callee on success.
+
+---+++ write([$path]) -> TotalCmd::FtpIni
+Write data to file. Returns false on failure, callee on succes.
+
+=cut
+###############################################################################
+
+sub read {
+    my ($self, $path) = @_;
+    $path //= $self->{ini}{path};
+    $self->{ini}->read($path);
+
+    $self->{general} = $self->{ini}->getSection('General');
+    $self->{certAlias} = $self->{ini}->getSection('CertAlias');
+    $self->{default} = $self->{ini}->getSection('default');
+    $self->{firewall} = $self->{ini}->getSection('firewall');
+    $self->{connections} = $self->{ini}->getList('connections');
+    $self->{oldConnections} = $self->{ini}->getList('OldConnections');
+
+    my $sessions = {};
+    foreach (@{$self->{connections}}) {
+        $sessions->{$_} = $self->{ini}{properties}{$_};
+    }
+    $self->{sessions} = $sessions;
+
+    return $self;
+}
+
+sub general {
+    $_[0]{general};
+}
+
+sub certAlias {
+    $_[0]{certAlias};
+}
+
+sub default {
+    $_[0]{default};
+}
+
+sub firewall {
+    $_[0]{firewall};
+}
+
+sub connections {
+    $_[0]{connections};
+}
+
+sub oldConnections {
+    $_[0]{oldConnections};
+}
+
+sub session {
+    $_[0]{sessions}{$_[1]};
+}
+
+1;
---
+++ lib/TotalCmd/Log/Entry.pm
@@ -0,0 +1,32 @@
+package TotalCmd::Log::Entry;
+
+use strict;
+use warnings;
+
+use Class::AccessorMaker {
+    date => undef,
+    time => undef,
+    operation => undef,
+    fsPluginOp => undef,
+    ok => undef,
+    error => undef,
+    skipped => undef,
+    user => undef,
+    encoding => undef,
+    source => undef,
+    destination => undef,
+};
+
+sub operationOk {
+    my $self = shift;
+    return ! $self->{error}
+        && ! $self->{skipped};
+}
+
+sub getOpMsg {
+    my $self = shift;
+    return $self->{error}
+        || $self->{skipped} && "Skipped";
+}
+
+1;
