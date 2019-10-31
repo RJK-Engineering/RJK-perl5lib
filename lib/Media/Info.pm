aa53a46891cd3ed6291c47b78ebbb2be8652490b
RJK-Engineering <rjklinkhamer@gmail.com>
2019-11-01 00:02:44 +0100

    added Media::Info

---
+++ lib/Media/Info.pm
@@ -0,0 +1,16 @@
+package Media::Info;
+
+use strict;
+use warnings;
+
+use Class::AccessorMaker {
+    fps => undef,
+    bitrate => undef,
+    duration => undef,
+    start => undef,
+    meta => undef,
+    video => undef,
+    audio => undef,
+};
+
+1;
