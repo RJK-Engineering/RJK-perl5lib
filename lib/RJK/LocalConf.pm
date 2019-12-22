670620b826af145343ea12c9234c67f97146e662
RJK-Engineering <rjklinkhamer@gmail.com>
2019-12-22 21:22:53 +0100

    added pod

--- lib/RJK/LocalConf.pm
+++ lib/RJK/LocalConf.pm
@@ -3,6 +3,19 @@ package RJK::LocalConf;
 use strict;
 use warnings;
 
+###############################################################################
+=pod
+
+---+++ GetOptions($filename, %defaultOptions) -> %options or \%options
+Load options from file(s) stored in local data directory =$ENV{APPDATA}= and/or
+=$ENV{LOCALAPPDATA}= where =$ENV{LOCALAPPDATA}= overrules =$ENV{APPDATA}=.
+   * =$filename= - path to config file relative to local data directory
+   * =%defaultOptions= - default options to return if not present in local config
+   * =%options= - option key/values
+
+=cut
+###############################################################################
+
 sub GetOptions {
     my $filename = shift;
     my %options = @_;
