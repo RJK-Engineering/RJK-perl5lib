471a91b76ad6d17363ea10c8ab8736606ec5df26
RJK-Engineering <rjklinkhamer@gmail.com>
2020-05-04 00:48:37 +0200

    added tests for RJK::File::PathUtils

---
+++ test/RJK/File/PathUtils/ExtractPath.pl
@@ -0,0 +1,13 @@
+use strict;
+use warnings;
+
+use File::PathUtils qw(ExtractPath);
+
+print ExtractPath('ksdjf=c:sdf'), "\n";
+print ExtractPath(' c:/sdf'), "\n";
+print ExtractPath(' c:/sdf//'), "\n";
+print ExtractPath(' c:/sdf//ee\ggg'), "\n";
+print ExtractPath('ksdjf=/sdf'), "\n";
+print ExtractPath(' /sdf'), "\n";
+print ExtractPath(' /sdf//'), "\n";
+print ExtractPath(' /sdf//ee\ggg'), "\n";
---
+++ test/RJK/File/PathUtils/RenamedString.pl
@@ -0,0 +1,20 @@
+use strict;
+use warnings;
+
+use File::PathUtils qw(RenamedString);
+
+testRenamedString("notes/2.txt", "notes/3.txt", "notes/{2.txt => 3.txt}");
+testRenamedString("notes/3.txt", "bandzak/src/3.txt", "{notes => bandzak/src}/3.txt");
+testRenamedString("bandzak/src/3.txt", "bandzak/src/nl/3.txt", "bandzak/src/{ => nl}/3.txt");
+testRenamedString("notes/3.txt", "notes/2.txt", "notes/{3.txt => 2.txt}");
+testRenamedString("bandzak/src/3.txt", "notes/3.txt", "{bandzak/src => notes}/3.txt");
+testRenamedString("bandzak/src/nl/3.txt", "bandzak/src/3.txt", "bandzak/src/{nl => }/3.txt");
+
+sub testRenamedString {
+    my ($from, $to, $string) = @_;
+    my $ren = RenamedString($from, $to);
+    print "$from => $to\n$ren\n$string\n\n";
+    if ($ren ne $string) {
+        die "Incorrect string";
+    }
+}
