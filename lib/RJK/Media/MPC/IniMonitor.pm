c4980fc62f2d0d727425b074601e3779f6724b73
RJK-Engineering <rjklinkhamer@gmail.com>
2020-05-16 11:19:20 +0200

    added getIniSection() and getIni()

--- lib/RJK/Media/MPC/IniMonitor.pm
+++ lib/RJK/Media/MPC/IniMonitor.pm
@@ -1,14 +1,28 @@
 package RJK::Media::MPC::IniMonitor;
-use parent 'RJK::Media::MPC::Monitor';
+use parent qw(RJK::Util::FileMonitor RJK::Media::MPC::Monitor);
 
 use strict;
 use warnings;
+
+use RJK::Util::Ini;
 
 sub new {
     my $self = bless {}, shift;
     my %opts = @_;
     $self->setFile($opts{file});
     return $self;
+}
+
+sub getIniSection {
+    my ($self, $payload, $section) = @_;
+    my $ini = $self->getIni($payload);
+    return $ini->getSection($section);
+}
+
+sub getIni {
+    my ($self, $payload) = @_;
+    return $payload->{ini} if $payload->{ini};
+    return $payload->{ini} = new RJK::Util::Ini($payload->{file})->read;
 }
 
 1;
