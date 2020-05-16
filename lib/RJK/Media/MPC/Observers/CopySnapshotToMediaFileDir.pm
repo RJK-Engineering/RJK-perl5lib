4287364f386477af127d08bcd4bd9c5bb531b13e
RJK-Engineering <rjklinkhamer@gmail.com>
2020-05-16 10:01:38 +0200

    Create CopySnapshotToMediaFileDir.pm

---
+++ lib/RJK/Media/MPC/Observers/CopySnapshotToMediaFileDir.pm
@@ -0,0 +1,22 @@
+package RJK::Media::MPC::Observers::CopySnapshotToMediaFileDir;
+use parent 'RJK::Media::MPC::Observer';
+
+sub handleSnapshotCreatedEvent {
+    my ($self, $snapshot, $monitor) = @_;
+
+    #~ my $mediaFile = $monitor->getMediaFile($snapshot);
+    my $mediaFile = $self->utils->getMediaFile($snapshot);
+
+    if ($mediaFile->{dir}) {
+        my $file = "$monitor->{snapshotDir}\\$snapshot->{filename}";
+        if (File::Copy::copy($file, $mediaFile->{dir})) {
+            print "Copied $mediaFile->{name}\n";
+        } else {
+            print "$!: $file -> $mediaFile->{dir}\n";
+        }
+    } else {
+        print "Media file directory not found.\n";
+    }
+}
+
+1;
