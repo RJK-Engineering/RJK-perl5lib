e2460a7195b2a7f1cccc922d0a84471e7542d4c4
RJK-Engineering <rjklinkhamer@gmail.com>
2020-05-24 18:17:51 +0200

    added FindLocalFile() and FindProgramDir(), removed rel2abs

--- lib/RJK/File/PathFinder.pm
+++ lib/RJK/File/PathFinder.pm
@@ -10,15 +10,12 @@ package RJK::File::PathFinder;
 use strict;
 use warnings;
 
-use File::Spec;
-
 ###############################################################################
 =pod
 
 ---++ FindPath(@paths) -> $path
 Find first existing path.
 With environment variable substitution.
-Relative paths are translated to absolute paths.
 
 =cut
 ###############################################################################
@@ -32,8 +29,49 @@ sub FindPath {
         $path = $_;
         last;
     }
-    $path = File::Spec->rel2abs($path) if $path;
     return $path;
+}
+
+###############################################################################
+=pod
+
+---+++ FindLocalFile($relativeFilePath) -> $path
+Find file stored in local data directory =$ENV{LOCALAPPDATA}= or
+=$ENV{APPDATA}= (in order of precedence).
+   * =$relativeFilePath= - path to file relative to local data directory
+
+=cut
+###############################################################################
+
+sub FindLocalFile {
+    my $relativeFilePath = shift;
+
+    # local conf, overrules roaming conf
+    my $path = "$ENV{LOCALAPPDATA}/$relativeFilePath";
+    return $path if -e $path;
+
+    # roaming conf
+    $path = "$ENV{APPDATA}/$relativeFilePath";
+    return $path if -e $path;
+}
+
+###############################################################################
+=pod
+
+---+++ FindProgramDir($relativeDirPath) -> $path
+Find program directory.
+
+=cut
+###############################################################################
+
+sub FindProgramDir {
+    my $relativeDirPath = shift;
+
+    my $path = "$ENV{ProgramW6432}/$relativeDirPath";
+    return $path if -e $path;
+
+    $path = "$ENV{ProgramFiles}/$relativeDirPath";
+    return $path if -e $path;
 }
 
 1;
