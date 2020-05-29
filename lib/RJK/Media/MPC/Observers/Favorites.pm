2979e4a80e381f856350c6f9de12ae0e68f199e7
RJK-Engineering <rjklinkhamer@gmail.com>
2020-05-29 18:35:10 +0200

    get favorites section from ini

--- lib/RJK/Media/MPC/Observers/Favorites.pm
+++ lib/RJK/Media/MPC/Observers/Favorites.pm
@@ -6,12 +6,21 @@ use warnings;
 
 sub handleFileChangedEvent {
     my ($self, $ini, $monitor) = @_;
-    print "Favorites TODO\n";
 
-    my $favorites = $monitor->getIniSection($ini, "Favorites\\Files");
+    my $favorites = $self->{favorites};
+    $self->getFavorites($ini, $monitor);
+    return if ! $favorites;
+    return if $favorites eq $self->{favorites};
+
+    # compare $favorites $self->{favorites}
 
     use Data::Dump;
     dd $favorites;
+}
+
+sub getFavorites {
+    my ($self, $ini, $monitor) = @_;
+    $self->{favorites} = $monitor->getIniSection($ini, "Favorites\\Files");
 }
 
 1;
