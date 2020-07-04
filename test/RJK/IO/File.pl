c8c1a9bfe964b3642bb4bd3a95743e77520d81fd
RJK-Engineering <rjklinkhamer@gmail.com>
2020-07-04 02:35:28 +0200

    restructured RJK:IO::File, added test

---
+++ test/RJK/IO/File.pl
@@ -0,0 +1,37 @@
+use strict;
+use warnings;
+
+use RJK::IO::File;
+
+my $dir = 'c:\temp';
+my $f = new RJK::IO::File($dir);
+
+my @f = $f->filenames(sub {-f "$dir/$_[0]"});
+foreach (@f) {
+    print "$_\n";
+}
+
+@f = $f->files(sub {$_[0]->isFile});
+foreach (@f) {
+    print "$_->{path}\n";
+}
+
+my $file = 'c:\temp\File.pl.test.file';
+$f = new RJK::IO::File($file);
+$f->createNewFile();
+$f->delete();
+
+my $p = $f->getParentFile;
+print "$p->{path}\n";
+$p = $p->getParentFile;
+print "$p->{path}\n";
+$p = $p->getParentFile;
+print defined $p ? "defined\n" : "not defined\n";
+
+$dir = 'c:\temp\File.pl.test.dir';
+$f = new RJK::IO::File($dir);
+use File::Path ();
+if (! $f->exists) {
+    File::Path::make_path($dir) or die "$!: $dir";
+    $f->delete();
+}
