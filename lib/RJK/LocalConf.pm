052f4cd720fcf3e1d4238390c54fc8ac245ac043
RJK-Engineering <rjklinkhamer@gmail.com>
2020-08-15 20:06:02 +0200

    use RJK::Util::Properties

--- lib/RJK/LocalConf.pm
+++ lib/RJK/LocalConf.pm
@@ -9,6 +9,8 @@ package RJK::LocalConf;
 use strict;
 use warnings;
 
+use RJK::Util::Properties;
+
 ###############################################################################
 =pod
 
@@ -41,44 +43,12 @@ sub GetOptions {
 
 sub loadConf {
     my ($path, $options) = @_;
-    my $propertiesFormat = $path =~ /\.properties$/i;
-    my $eat;
 
-    open my $fh, '<', $path or die "$!";
-    while (<$fh>) {
-        s/^\s+//;                                           # trim leading space
-        next unless /./;                                    # skip empty line
-        next if /^[!#]/;                                    # skip comments
-        s/\s+$//;                                           # trim trailing space
+    my $props = new RJK::Util::Properties();
+    $props->load($path);
 
-        if ($eat) {
-            my $option = $eat;
-            $eat = s/\\$//;                                 # remove backslash
-
-            if (! $propertiesFormat) {
-                s/\s+$//;                                   # trim trailing space after removing backslash
-                $options->{$option} .= "\n";
-            }
-            $options->{$option} .= $_;
-
-            $eat = $option if $eat;
-            next;
-        }
-        $eat = s/\\$//;                                     # remove backslash
-
-        my @option = split /=/, $_, 2;
-        $option[1] =~ s/^\s+//;                             # trim leading space
-        $option[0] =~ s/\s+$//;                             # trim trailing space
-        $option[0] =~ s/\.(\w?)/\U$1/g;                     # make camelCase
-        if ($propertiesFormat) {
-            $option[1] =~ s/\\([rnt'"\\])/"qq|\\$1|"/gee;   # escaped characters
-        } elsif ($eat) {
-            $option[1] =~ s/\s+$//;                         # trim trailing space after removing backslash
-        }
-
-        $options->{$option[0]} = $option[1];
-
-        $eat = $option[0] if $eat;
+    while (my ($k, $v) = each %{$props->hash}) {
+        $options->{$k} = $v;
     }
 }
 
