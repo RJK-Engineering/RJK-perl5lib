d4c693097838e976f5c3de3a30cee3b593cb4880
RJK-Engineering <rjklinkhamer@gmail.com>
2020-08-20 03:33:05 +0200

    Create Config.pm

---
+++ lib/RJK/Filecheck/Config.pm
@@ -0,0 +1,26 @@
+package RJK::Filecheck::Config;
+
+use strict;
+use warnings;
+
+use RJK::Util::Properties;
+
+my $config;
+
+sub get {
+    my ($class, $prop) = @_;
+    $class->loadConfig() if ! $config;
+    die "Missing configuration property: $prop" if ! $config->has($prop);
+    return $config->get($prop);
+}
+
+sub loadConfig {
+    $config = new RJK::Util::Properties();
+    if ($ENV{FILECHECK_CONF_FILE}) {
+        $config->load($ENV{FILECHECK_CONF_FILE});
+    } else {
+        $config->load("$ENV{LOCALAPPDATA}/filecheck.properties");
+    }
+}
+
+1;
