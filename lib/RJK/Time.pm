e6c82b3ee289e09e3a5d5641ed70ad2ddf153887
RJK-Engineering <rjklinkhamer@gmail.com>
2020-11-03 01:01:11 +0100

    Update Time.pm

--- lib/RJK/Time.pm
+++ lib/RJK/Time.pm
@@ -7,6 +7,11 @@ sub seconds {
     $_[0]{seconds};
 }
 
+sub plus {
+    my ($self, $other) = @_;
+    bless { seconds => $self->{seconds} + $other->{seconds} }, __PACKAGE__;
+}
+
 sub minus {
     my ($self, $other) = @_;
     bless { seconds => $self->{seconds} - $other->{seconds} }, __PACKAGE__;
