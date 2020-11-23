641486382e1bd67fa9e76b41ad3de5e75a6ef182
RJK-Engineering <rjklinkhamer@gmail.com>
2020-11-23 16:26:44 +0100

    Update Observer.pm

--- lib/RJK/Media/MPC/Observer.pm
+++ lib/RJK/Media/MPC/Observer.pm
@@ -6,12 +6,25 @@ use warnings;
 sub new {
     my $self = bless {}, shift;
     $self->{name} = shift;
-    $self->{utils} = shift;
+    $self->{controller} = shift;
+    $self->{actions} = $self->setupActions($self->{controller});
     return $self;
 }
 
+sub setupActions {
+}
+
+sub do {
+    my ($self, $action) = @_;
+    $self->{actions}->$action();
+}
+
+sub actions {
+    $_[0]{actions}
+}
+
 sub utils {
-    $_[0]{utils}
+    $_[0]{controller}{utils}
 }
 
 1;
