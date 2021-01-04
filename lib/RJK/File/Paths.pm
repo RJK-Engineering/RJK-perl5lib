7310333ae9358a23ccdd347ca5079c73654625e2
RJK-Engineering <rjklinkhamer@gmail.com>
2021-01-04 19:57:36 +0100

    Update Paths.pm

--- lib/RJK/File/Paths.pm
+++ lib/RJK/File/Paths.pm
@@ -3,19 +3,21 @@ package RJK::File::Paths;
 use strict;
 use warnings;
 
-use File::Spec::Functions qw(canonpath catdir catpath splitpath);
+use File::Spec::Functions qw(catdir catpath splitpath);
 
 sub get {
     my $path = catdir(grep {$_ ne ""} @_);
 
     my ($volume, $directories, $file) = splitpath($path);
+    $directories =~ s/\\$//;
     my ($basename, $extension) = ($file =~ /^(.+)\.(.+)$/);
 
     return bless {
         path => $path,
-        dir => $file eq '' ? '' : catpath($volume, $directories, ''),
+        parent => $file eq '' ? '' : catpath($volume, $directories, ''),
         name => $file,
         volume => $volume,
+        drive => substr($volume, 0, 1),
         directories => $directories,
         basename => $basename // $file,
         extension => $extension // ''
