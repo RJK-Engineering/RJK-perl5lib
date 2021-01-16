e7d954b990dc3b9378742087bb27caaf5b5eb94f
RJK-Engineering <rjklinkhamer@gmail.com>
2021-01-16 12:39:04 +0100

    Update SimpleFileVisitor.pl

--- test/RJK/SimpleFileVisitor.pl
+++ test/RJK/SimpleFileVisitor.pl
@@ -1,79 +1,15 @@
-use strict;
-use warnings;
-
-use RJK::Files;
-use RJK::SimpleFileVisitor;
-use RJK::File::TreeVisitResult;
-
-my $visitor = new RJK::SimpleFileVisitor(
-    visitFileFailed => sub {
-        my ($file, $error) = @_;
-        print "$error: $file->{path}\n";
-        #~ return TERMINATE;
-    },
-    preVisitDir => sub {
-        my ($dir, $stat) = @_;
-        printf ">D %s\n", $dir->{path};
-        #~ return TERMINATE;
-        #~ return SKIP_SUBTREE if $dir->{name} eq 'dir1';
-        #~ return SKIP_SIBLINGS if $dir->{name} eq 'dir1';
-        #~ return SKIP_FILES if $dir->{name} eq 'dir1';
-        #~ return SKIP_DIRS if $dir->{name} eq 'dir1';
-    },
-    postVisitDir => sub {
-        my ($dir, $stat) = @_;
-        printf "<D %s\n", $dir->{path};
-        #~ return TERMINATE;
-        #~ return SKIP_SIBLINGS if $dir->{name} eq 'dir1';
-    },
-    postVisitFiles => sub {
-        my ($dir, $stat) = @_;
-        printf "<F %s\n", $dir->{path};
-        #~ return TERMINATE;
-        #~ return SKIP_SIBLINGS if $dir->{name} eq 'dir1';
-        #~ return SKIP_DIRS if $dir->{name} eq 'dir1';
-    },
-    visitFile => sub {
-        my ($file, $stat) = @_;
-        print "   $file->{path}\n";
-        #~ return TERMINATE;
-        #~ return SKIP_SIBLINGS if $file->{name} eq 'file1';
-        #~ return SKIP_SIBLINGS if $file->{name} eq 'file2';
-        #~ return SKIP_FILES if $file->{name} eq 'file1';
-    }
-);
-
-my $testDir = 'c:\temp\root';
-createFileTree($testDir);
-
-my $path = $testDir;
-#~ my $path = 'fail';
-
-my $terminated = RJK::Files->traverse($path, $visitor, {sort=>1});
-print "TERMINATEd\n" if $terminated;
-
-sub createFileTree {
-    mkdir $testDir;
-    mkdir "$testDir/dir1";
-    mkdir "$testDir/dir1/dir2";
-    mkdir "$testDir/dir3";
-    createFile("$testDir/file1");
-    createFile("$testDir/dir1/file2");
-}
-
-sub createFile {
-    my $file = shift;
-    open my $fh, '>', $file or return;
-    close $fh;
-}
-
-__END__
-
+use Data::Dump;
 use RJK::File::Stats;
-my $stats = RJK::File::Stats->traverse($path);
+use RJK::SimpleFileVisitor;
+
+my $testDir = 'c:\temp\1';
+die "Testdir does not exist: $testDir" if !-e $testDir;
+
+my $stats = RJK::File::Stats->traverse($testDir);
+dd $stats;
 
 $stats = RJK::File::Stats->createStats();
-$visitor = new RJK::SimpleFileVisitor(
+my $visitor = new RJK::SimpleFileVisitor(
     visitFileFailed => sub {
         my ($file, $error) = @_;
         print "$error: $file->{path}\n";
@@ -86,9 +22,8 @@ $visitor = new RJK::SimpleFileVisitor(
         printf "dirs: %u, files: %u, size: %u\n", $ds->{dirs}, $ds->{files}, $ds->{size};
     }
 );
-#~ RJK::File::Stats->traverse($path, undef, undef, $stats);
-RJK::File::Stats->traverse($path, $visitor, undef, $stats);
-use Data::Dump;
+#~ RJK::File::Stats->traverse($testDir, undef, undef, $stats);
+RJK::File::Stats->traverse($testDir, $visitor, undef, $stats);
 dd $stats;
 
 __END__
