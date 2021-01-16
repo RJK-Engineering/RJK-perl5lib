2f7e14e82a5fb8e58f9103b7b776e2371801e425
RJK-Engineering <rjklinkhamer@gmail.com>
2021-01-16 10:47:18 +0100

    Create TreeVisitResult.pl

---
+++ test/RJK/File/TreeVisitResult.pl
@@ -0,0 +1,19 @@
+use strict;
+use warnings;
+
+use RJK::File::TreeVisitResult qw(:constants matchesTreeVisitResult);
+
+my $result = undef;
+print "fail\n" if matchesTreeVisitResult($result, TERMINATE, SKIP_SIBLINGS, SKIP_SUBTREE);
+
+$result = CONTINUE;
+print "fail\n" if matchesTreeVisitResult($result, TERMINATE, SKIP_SIBLINGS, SKIP_SUBTREE);
+
+$result = SKIP_SIBLINGS;
+print "ok\n" if matchesTreeVisitResult($result, TERMINATE, SKIP_SIBLINGS, SKIP_SUBTREE);
+
+$result = SKIP_FILES;
+print "ok\n" if matchesTreeVisitResult($result, SKIP_FILES);
+
+$result = SKIP_DIRECTORIES;
+print "ok\n" if matchesTreeVisitResult($result, SKIP_DIRECTORIES);
