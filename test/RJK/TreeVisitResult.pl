ad168dd2b4fef3ae9d61bda7d3956381dbe33e6a
RJK-Engineering <rjklinkhamer@gmail.com>
2021-01-16 10:43:22 +0100

    Update TreeVisitResult.pl

--- test/RJK/TreeVisitResult.pl
+++ test/RJK/TreeVisitResult.pl
@@ -1,13 +1,13 @@
 use strict;
 use warnings;
 
-use RJK::TreeVisitResult;
+use RJK::TreeVisitResult qw(:constants matchesTreeVisitResult);
 
 my $result = undef;
-print "fail\n" if RJK::TreeVisitResult::isResult($result, TERMINATE, SKIP_SIBLINGS, SKIP_SUBTREE);
+print "fail\n" if matchesTreeVisitResult($result, TERMINATE, SKIP_SIBLINGS, SKIP_SUBTREE);
 
 $result = CONTINUE;
-print "fail\n" if RJK::TreeVisitResult::isResult($result, TERMINATE, SKIP_SIBLINGS, SKIP_SUBTREE);
+print "fail\n" if matchesTreeVisitResult($result, TERMINATE, SKIP_SIBLINGS, SKIP_SUBTREE);
 
 $result = SKIP_SIBLINGS;
-print "ok\n" if RJK::TreeVisitResult::isResult($result, TERMINATE, SKIP_SIBLINGS, SKIP_SUBTREE);
+print "ok\n" if matchesTreeVisitResult($result, TERMINATE, SKIP_SIBLINGS, SKIP_SUBTREE);
