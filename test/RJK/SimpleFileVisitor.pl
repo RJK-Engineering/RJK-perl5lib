4256b8d199e0c44d23c38f44616c9bf0daacd42a
RJK-Engineering <rjklinkhamer@gmail.com>
2021-01-16 11:44:25 +0100

    Update SimpleFileVisitor.pl

--- test/RJK/SimpleFileVisitor.pl
+++ test/RJK/SimpleFileVisitor.pl
@@ -9,6 +9,7 @@ my $visitor = new RJK::SimpleFileVisitor(
     visitFileFailed => sub {
         my ($file, $error) = @_;
         print "$error: $file->{path}\n";
+        #~ return TERMINATE;
     },
     preVisitDir => sub {
         my ($dir, $stat) = @_;
@@ -16,6 +17,8 @@ my $visitor = new RJK::SimpleFileVisitor(
         #~ return TERMINATE;
         #~ return SKIP_SUBTREE if $dir->{name} eq 'dir1';
         #~ return SKIP_SIBLINGS if $dir->{name} eq 'dir1';
+        #~ return SKIP_FILES if $dir->{name} eq 'dir1';
+        #~ return SKIP_DIRS if $dir->{name} eq 'dir1';
     },
     postVisitDir => sub {
         my ($dir, $stat) = @_;
@@ -28,22 +31,41 @@ my $visitor = new RJK::SimpleFileVisitor(
         printf "<F %s\n", $dir->{path};
         #~ return TERMINATE;
         #~ return SKIP_SIBLINGS if $dir->{name} eq 'dir1';
+        #~ return SKIP_DIRS if $dir->{name} eq 'dir1';
     },
     visitFile => sub {
         my ($file, $stat) = @_;
-        print "$file->{path}\n";
+        print "   $file->{path}\n";
         #~ return TERMINATE;
         #~ return SKIP_SIBLINGS if $file->{name} eq 'file1';
         #~ return SKIP_SIBLINGS if $file->{name} eq 'file2';
+        #~ return SKIP_FILES if $file->{name} eq 'file1';
     }
 );
 
 my $testDir = 'c:\temp\root';
+createFileTree($testDir);
+
 my $path = $testDir;
 #~ my $path = 'fail';
 
 my $terminated = RJK::Files->traverse($path, $visitor, {sort=>1});
 print "TERMINATEd\n" if $terminated;
+
+sub createFileTree {
+    mkdir $testDir;
+    mkdir "$testDir/dir1";
+    mkdir "$testDir/dir1/dir2";
+    mkdir "$testDir/dir3";
+    createFile("$testDir/file1");
+    createFile("$testDir/dir1/file2");
+}
+
+sub createFile {
+    my $file = shift;
+    open my $fh, '>', $file or return;
+    close $fh;
+}
 
 __END__
 
