18ef31f14539dd1d42bcd454c816704662b8e53d
RJK-Engineering <rjklinkhamer@gmail.com>
2021-01-16 18:34:52 +0100

    Update Paths.pm

--- lib/RJK/File/Paths.pm
+++ lib/RJK/File/Paths.pm
@@ -3,21 +3,28 @@ package RJK::File::Paths;
 use strict;
 use warnings;
 
-use File::Spec::Functions qw(catdir catpath splitpath);
+my $separatorsRegex = qr{ [\\\/]+ }x;
+my $splitPathRegex = qr{ ^ ((\w):) (?: (\\.+)\\(.+) | \\(.*) )? $ }x;
+my $splitFilenameRegex = qr{ ^ (.+)\.(.+) $ }x;
 
 sub get {
-    my $path = catdir(grep {$_ ne ""} @_);
+    my $path = ucfirst join "\\", @_;
+    $path =~ s/$separatorsRegex/\\/g;
+    $path =~ s/$separatorsRegex$//;
 
-    my ($volume, $directories, $file) = splitpath($path);
-    $directories =~ s/\\$//;
-    my ($basename, $extension) = ($file =~ /^(.+)\.(.+)$/);
+    my ($volume, $drive, $directories, $file, $noparent) = $path =~ /$splitPathRegex/;
+    if (not defined $file) {
+        $file = $noparent // '';
+        $directories = '';
+    }
+    my ($basename, $extension) = ($file =~ /$splitFilenameRegex/);
 
     return bless {
         path => $path,
-        parent => $file eq '' ? '' : catpath($volume, $directories, ''),
+        parent => $file eq '' ? '' : $volume.$directories,
         name => $file,
         volume => $volume,
-        drive => substr($volume, 0, 1),
+        drive => $drive,
         directories => $directories,
         basename => $basename // $file,
         extension => $extension // ''
