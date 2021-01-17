445f14decd07bc04f492c1ed90ba68ae23262a2b
RJK-Engineering <rjklinkhamer@gmail.com>
2021-01-17 10:09:23 +0100

    Update Stats.pm

--- lib/RJK/File/Stats.pm
+++ lib/RJK/File/Stats.pm
@@ -8,10 +8,10 @@ use RJK::File::TraverseStats;
 use RJK::FileVisitor::StatsWrapper;
 
 sub traverse {
-    my ($class, $path, $visitor, $opts, $totals) = @_;
+    my ($self, $path, $visitor, $opts, $totals) = @_;
     $visitor ||= bless {}, 'RJK::FileVisitor';
 
-    my $stats = $class->createStats();
+    my $stats = $self->createStats();
     $visitor = new RJK::FileVisitor::StatsWrapper($visitor, $stats);
     RJK::Files->traverse($path, $visitor, $opts);
 
