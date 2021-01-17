e5fe735cdd72dff73ed7cd6e8534eae386969565
RJK-Engineering <rjklinkhamer@gmail.com>
2021-01-17 10:09:39 +0100

    Update Filecheck.pm

--- lib/RJK/Filecheck.pm
+++ lib/RJK/Filecheck.pm
@@ -6,27 +6,12 @@ use warnings;
 use Module::Load;
 
 my $store;
-my $storeModule = "RJK::Filecheck::Store";
-
-sub getDrives {
-    getStore()->getDrives();
-}
-
-sub getBackupDirs {
-    getStore()->getBackupDirs();
-}
-
-sub storeBackupDirs {
-    my ($class, $list) = @_;
-    getStore()->storeBackupDirs($list);
-}
 
 sub getStore {
-    if (! $store) {
-        load $storeModule;
-        $store = $storeModule;
-    }
-    return $store;
+    my ($self, $module) = @_;
+    return $store->{$module} if $store->{$module};
+    load $module;
+    return $store->{$module} = $module;
 }
 
 1;
