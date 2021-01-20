5b8f393f7bfa48eb671516fe9e121f7c9b69d5f7
RJK-Engineering <rjklinkhamer@gmail.com>
2021-01-20 10:56:39 +0100

    Update Sites.pm

--- lib/RJK/Sites.pm
+++ lib/RJK/Sites.pm
@@ -4,22 +4,36 @@ use strict;
 use warnings;
 
 use RJK::Util::JSON;
+use RJK::Filecheck::Config;
 
 my $sites;
 
-sub get {
+sub all {
     my ($class, $url) = @_;
-    _getSites();
-    my @sites = values %$sites;
-    foreach my $site (@sites) {
+    &_getSites;
+    return wantarray ? values %$sites : $sites;
+}
+
+sub get {
+    my ($class, $name) = @_;
+    &_getSites;
+    return $sites->{$name};
+}
+
+sub getForUrl {
+    my ($class, $url) = @_;
+    &_getSites;
+
+    foreach my $site (values %$sites) {
         return $site if $url =~ /$site->{host}/i;
     }
 }
 
 sub getForId {
     my ($class, $id) = @_;
-    _getSites();
+    &_getSites;
     my @sites = sort { ($a->{idRegexPrio}//999) <=> ($b->{idRegexPrio}//999) } values %$sites;
+
     foreach my $site (@sites) {
         next if ! $site->{idRegex};
         return $site if $id =~ /^$site->{idRegex}$/;
@@ -28,9 +42,10 @@ sub getForId {
 
 sub _getSites {
     return $sites if $sites;
-    my $confFilePath = "%LOCALAPPDATA%\\RJK-utils\\sites.json";
-    $confFilePath =~ s/%(.+)%/$ENV{$1}/g;
+
+    my $confFilePath = RJK::Filecheck::Config->get('sites.conf.file');
     $sites = RJK::Util::JSON->read($confFilePath);
+
     while (my ($id, $site) = each %$sites) {
         $site->{id} = $id;
         bless $site, "RJK::Site";
