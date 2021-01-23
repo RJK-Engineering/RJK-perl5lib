9a6de1ff1614bc1e810c550ac4385f5519e232c0
RJK-Engineering <rjklinkhamer@gmail.com>
2021-01-23 14:14:41 +0100

    Update Log.pm

--- lib/RJK/Log.pm
+++ lib/RJK/Log.pm
@@ -4,6 +4,8 @@ use strict;
 use warnings;
 
 use RJK::Env;
+use RJK::Path;
+use RJK::Paths;
 use Log::Log4perl;
 
 our $conf;
@@ -20,16 +22,13 @@ sub config {
 
 sub logWarnings {
     my ($self, %opts) = @_;
-    if (! $Log::Log4perl::Logger::INITIALIZED) {
-        Log::Log4perl::Logger->init_warn;
-        return;
-    }
+    _init($opts{conf}) if ! $Log::Log4perl::Logger::INITIALIZED;
     my $logger = Log::Log4perl->get_logger($opts{logger} // 'warnings');
     my $existingHandler = $SIG{__WARN__} unless $opts{replaceHandler};
 
     $SIG{__WARN__} = sub {
         local $Log::Log4perl::caller_depth = $Log::Log4perl::caller_depth + 1;
-        $logger->warn($_[0]);
+        $logger->warn($_[0] =~ s/\v+$//r);
         warn $_[0] unless $opts{quiet};
         $existingHandler->(@_) if $existingHandler;
     };
@@ -37,13 +36,13 @@ sub logWarnings {
 
 sub logDie {
     my ($self, %opts) = @_;
-    _init() if ! $Log::Log4perl::Logger::INITIALIZED;
+    _init($opts{conf}) if ! $Log::Log4perl::Logger::INITIALIZED;
     my $logger = Log::Log4perl->get_logger($opts{logger} // 'die');
     my $existingHandler = $SIG{__DIE__} unless $opts{replaceHandler};
 
     $SIG{__DIE__} = sub {
         local $Log::Log4perl::caller_depth = $Log::Log4perl::caller_depth + 1;
-        $logger->fatal($_[0]);
+        $logger->fatal($_[0] =~ s/\v+$//r);
         $existingHandler->(@_) if $existingHandler;
     };
 }
@@ -65,6 +64,9 @@ sub _init {
 }
 
 sub localConfFile {
+    my $appName = RJK::Paths->get($0)->basename;
+    (RJK::Env->findLocalFiles("$appName/log4perl.conf"))[0]
+    ||
     (RJK::Env->findLocalFiles('log4perl.conf'))[0];
 }
 
