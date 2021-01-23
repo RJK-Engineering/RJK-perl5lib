9e5e1f6561448f31ca7e4a85702747c30c5c3f37
RJK-Engineering <rjklinkhamer@gmail.com>
2021-01-23 01:51:51 +0100

    Update JSON.pl

--- test/RJK/Util/JSON.pl
+++ test/RJK/Util/JSON.pl
@@ -3,15 +3,24 @@ use warnings;
 
 use RJK::Util::JSON;
 
-my $file = 'JSON.pm.test.json';
-open my $fh, '>', $file or die "$!";
-print $fh '{ "key": [ "value1", "value2" ] }';
-close $fh;
+my $testDir = 'c:\temp\root';
+my $file = "$testDir/JSON.pm.test.json";
+createFile($file, '{ "key": [ "value1", "value2" ] }');
 
 my $data = RJK::Util::JSON->read($file);
 use Data::Dump;
 dd $data;
+unlink $file;
 
-my $file2 = 'JSON.pm.test2.json';
+$file = "$testDir/JSON.pm.test2.json";
 $data = { key => [ qw(value1 value2) ] };
-RJK::Util::JSON->write($file2, $data);
+RJK::Util::JSON->write($file, $data);
+RJK::Util::JSON->read($file);
+unlink $file;
+
+sub createFile {
+    my ($file, $content) = @_;
+    open my $fh, '>', $file or die "$!: $file";
+    print $fh $content;
+    close $fh;
+}
