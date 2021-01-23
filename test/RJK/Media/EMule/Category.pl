3ffc11611a6d7210ac1401b6e9fbb3322796c83e
RJK-Engineering <rjklinkhamer@gmail.com>
2021-01-23 13:25:49 +0100

    Update Category.pl

--- test/RJK/Media/EMule/Category.pl
+++ test/RJK/Media/EMule/Category.pl
@@ -9,7 +9,7 @@ $opts{categoryIni} = '';
 
 if (! $opts{categoryIni}) {
     my $file = 'eMule\config\Category.ini';
-    $opts{categoryIni} = RJK::Env->findLocalFile($file)
+    $opts{categoryIni} = (RJK::Env->findLocalFiles($file))[0]
         or die "Category.ini not found";
 }
 
