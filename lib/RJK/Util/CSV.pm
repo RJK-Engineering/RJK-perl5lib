691777f1e9ae38ca356778680e42c93a4e412d8c
RJK-Engineering <rjklinkhamer@gmail.com>
2021-01-27 00:16:45 +0100

    Create CSV.pm

---
+++ lib/RJK/Util/CSV.pm
@@ -0,0 +1,37 @@
+package RJK::Util::CSV;
+
+use strict;
+use warnings;
+
+use Exceptions;
+use FileException;
+use OpenFileException;
+
+our $delimeter = ",";
+
+sub read {
+    my ($self, $file, $callback) = @_;
+    $callback //= sub {};
+    my @rows;
+    throw FileException(error => "Not a file", file => $file) if ! -f $file;
+
+    open my $fh, '<', $file or throw OpenFileException(file => $file, error => "$!");
+    while (<$fh>) {
+        chomp;
+        my $row = [split /$delimeter/];
+        push @rows, $row;
+        last if $callback->($row);
+    }
+    close $fh;
+
+    return \@rows;
+}
+
+sub write {
+    my ($self, $file, $rows) = @_;
+    open my $fh, '>', $file or throw OpenFileException(file => $file, error => "$!");
+    print $fh join $delimeter, @$_ for @$rows;
+    close $fh;
+}
+
+1;
