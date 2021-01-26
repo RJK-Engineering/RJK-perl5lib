9b776e2d8177e010d0e58f8a35e7b17744086535
RJK-Engineering <rjklinkhamer@gmail.com>
2021-01-27 00:08:28 +0100

    Update Snapshots.pm

--- lib/RJK/Filecheck/Snapshots.pm
+++ lib/RJK/Filecheck/Snapshots.pm
@@ -3,22 +3,13 @@ package RJK::Filecheck::Snapshots;
 use strict;
 use warnings;
 
+use RJK::Options::Util;
 use RJK::Filecheck::CreateSnapshotsVisitor;
-use RJK::Files;
-use RJK::ListFiles;
-
-my $opts;
 
 sub create {
-    my $self = shift;
-    $opts = shift;
+    my ($self, $opts) = @_;
     my $visitor = new RJK::Filecheck::CreateSnapshotsVisitor($opts);
-
-    if ($opts->{listFile}) {
-        RJK::ListFiles->traverse($opts->{listFile}, $visitor);
-    } elsif ($opts->{path}) {
-        RJK::Files->traverse($opts->{path}, $visitor);
-    }
+    RJK::Options::Util->traverseFiles($opts, $visitor);
 }
 
 1;
