5743cf2c3bcab42374d7aca1749570d533652798
RJK-Engineering <rjklinkhamer@gmail.com>
2021-01-30 20:30:05 +0100

    Update Util.pm

--- lib/RJK/Options/Util.pm
+++ lib/RJK/Options/Util.pm
@@ -13,6 +13,10 @@ sub traverseFiles {
         RJK::ListFiles->traverse($opts->{listFile}, $visitor, $opts);
     } elsif ($opts->{path}) {
         RJK::Files->traverse($opts->{path}, $visitor, $opts);
+    } elsif (@ARGV) {
+        RJK::Files->traverse($_, $visitor) for @ARGV;
+    } elsif ($opts->{stdin}) {
+        RJK::Files->traverse($_, $visitor) while <>;
     } else {
         RJK::Files->traverse(RJK::Cwd->get, $visitor, $opts);
     }
