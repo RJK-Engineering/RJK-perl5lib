d47fc39d5e2c47d9e39acc93b248f828c4498aaa
RJK-Engineering <rjklinkhamer@gmail.com>
2021-01-31 07:03:57 +0100

    Update Util.pm

--- lib/RJK/Options/Util.pm
+++ lib/RJK/Options/Util.pm
@@ -8,17 +8,17 @@ use RJK::Files;
 use RJK::ListFiles;
 
 sub traverseFiles {
-    my ($self, $opts, $visitor) = @_;
+    my ($self, $opts, $visitor, $stats) = @_;
     if ($opts->{listFile}) {
-        RJK::ListFiles->traverse($opts->{listFile}, $visitor, $opts);
+        RJK::ListFiles->traverse($opts->{listFile}, $visitor, $opts, $stats);
     } elsif ($opts->{path}) {
-        RJK::Files->traverse($opts->{path}, $visitor, $opts);
+        RJK::Files->traverse($opts->{path}, $visitor, $opts, $stats);
     } elsif (@ARGV) {
-        RJK::Files->traverse($_, $visitor) for @ARGV;
+        RJK::Files->traverse($_, $visitor, $opts, $stats) for @ARGV;
     } elsif ($opts->{stdin}) {
-        RJK::Files->traverse($_, $visitor) while <>;
+        RJK::Files->traverse($_, $visitor, $opts, $stats) while <>;
     } else {
-        RJK::Files->traverse(RJK::Cwd->get, $visitor, $opts);
+        RJK::Files->traverse(RJK::Cwd->get, $visitor, $opts, $stats);
     }
 }
 
