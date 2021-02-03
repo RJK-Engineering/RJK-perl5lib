66b0b7a57dcc0f7d91d1bb09d61f5d2a1d32d700
RJK-Engineering <rjklinkhamer@gmail.com>
2021-02-04 00:59:02 +0100

    Update Log.pm

--- lib/RJK/TotalCmd/Log.pm
+++ lib/RJK/TotalCmd/Log.pm
@@ -11,21 +11,13 @@ package RJK::TotalCmd::Log;
 use strict;
 use warnings;
 
-use Exception::Class (
-    'Exception',
-    'RJK::TotalCmd::Log::Exception' =>
-        { isa => 'Exception' },
-    'RJK::TotalCmd::Log::CorruptLineException' =>
-        { isa => 'Exception',
-          fields => [qw(line)] },
-);
-
-
-use RJK::TotalCmd::Log::Entry;
+use Exceptions;
+use FileException;
+use LineParseException;
 
 our @fields = qw(
     date time
-    sourcedir sourcefile source dir file path
+    sourcedir sourcefile source
     destdir destfile destination
     operation isFsPluginOp
     success error skipped
@@ -58,8 +50,8 @@ our @errors = (
    * =$opts{visitFailed}= - Optional. Traversal is stopped if this sub returns a true value.
      Subroutine arguments:
       * =$line= - the corrupt line.
-   * throws RJK::TotalCmd::Log::Exception
-   * throws RJK::TotalCmd::Log::CorruptLineException, unless =$visitFailed= is defined.
+   * throws FileException
+   * throws LineParseException, unless =$visitFailed= is defined.
 
 =cut
 ###############################################################################
@@ -68,111 +60,95 @@ sub traverse {
     my ($self, %opts) = @_;
 
     open my $fh, '<', $opts{file}
-        or throw RJK::TotalCmd::Log::Exception("$!");
+        or throw FileException(error => "$!", file => $opts{file});
 
-    $opts{visitEntry} && ref $opts{visitEntry} && ref $opts{visitEntry} eq "CODE"
-        or throw RJK::TotalCmd::Log::Exception("No callback");
+    $opts{visitFailed} //= sub {
+        throw LineParseException(
+            error => "Corrupt line: $_[0]",
+            file => $opts{file},
+            line => $_[0]
+        );
+    };
 
-    if ($opts{visitFailed}) {
-        ref $opts{visitFailed} && ref $opts{visitFailed} eq "CODE"
-            or throw RJK::TotalCmd::Log::Exception("Invalid corrupt line callback");
-    } else {
-        $opts{visitFailed} = sub {
-            my $line = shift;
-            throw RJK::TotalCmd::Log::CorruptLineException(
-                error => "Corrupt line: $line",
-                line => $line,
-            );
-        };
-    }
-
-    my $firstline = 1;
+    my $bom;
     while (<$fh>) {
+        $bom //= readUtf8Bom();
         next unless /\S/;
         chomp;
+
         my $line = $_;
-
-        # first line (sometimes?) starts with weird chars
-        if ($firstline) {
-            $firstline = 0;
-            if (/^.{1,3}(\d\d-\d\d-\d\d\d\d .*)/)  {
-                $_ = $1;
-            }
-        }
-
-        # Log entry format: [date and time]: [entry]
-        my $entry = new RJK::TotalCmd::Log::Entry;
-        if (s/^(\d\d-\d\d-\d\d\d\d) (\d\d:\d\d:\d\d): //) {
-            $entry->{date} = $1;
-            $entry->{time} = $2;
+        if (my $entry = &parseEntry) {
+            local $_ = $entry;
+            last if $opts{visitEntry}->($entry);
         } else {
             last if $opts{visitFailed}->($line);
-            next;
         }
-
-        # parse operation
-        # Success:  [op]: [params]
-        # Error:    [op](Error: [error]): [params]
-        # Skip:     [op](Skipped): [params]
-        # Startup:  Program start ([user]) [encoding]
-        # Shutdown: Program shutdown ([user])
-
-        # file system plugin operation
-        if (s/^FS://) {
-            $entry->{isFsPluginOp} = 1;
-        }
-
-        if (s/^(\w+): //) {
-            $entry->{operation} = $1;
-            $entry->{success} = 1;
-        } elsif (s/^(\w+)\(Error: (.+)\): //) {
-            $entry->{operation} = $1;
-            $entry->{error} = $2;
-        } elsif (s/^(\w+)\(Skipped\): //) {
-            $entry->{operation} = $1;
-            $entry->{skipped} = 1;
-        } elsif (/^Program start \((.+)\)$/) {
-            $entry->{operation} = "Startup";
-            $entry->{user} = $1;
-        } elsif (/^Program start \((.+)\) (.+)$/) {
-            $entry->{operation} = "Startup";
-            $entry->{user} = $1;
-            $entry->{encoding} = $2;
-        } elsif (/^Program shutdown \((.+)\)$/) {
-            $entry->{operation} = "Shutdown";
-            $entry->{user} = $1;
-        } else {
-            last if $opts{visitFailed}->($line);
-            next;
-        }
-
-        # parse params
-        if (/^((.+)\\(.+)) -> ((.+)\\(.+))$/) {
-            $entry->{source} = $1;
-            $entry->{sourcedir} = $2;
-            $entry->{sourcefile} = $3;
-            $entry->{destination} = $4;
-            $entry->{destdir} = $5;
-            $entry->{destfile} = $6;
-        } elsif (/^((.+)\\(.+))$/) {
-            $entry->{source} = $1;
-            $entry->{sourcedir} = $2;
-            $entry->{sourcefile} = $3;
-        } else {
-            $entry->{source} = $_;
-        }
-
-        # aliases
-        if ($entry->{sourcedir}) {
-            $entry->{path} = $entry->{source};
-            $entry->{dir} = $entry->{sourcedir};
-            $entry->{file} = $entry->{sourcefile};
-        }
-
-        local $_ = $entry;
-        last if $opts{visitEntry}->($entry);
     }
     close $fh;
+    throw FileException(error => "$!", file => $opts{file}) if $!;
+}
+
+sub parseEntry {
+    return if not s/^(\d\d-\d\d-\d\d\d\d) (\d\d:\d\d:\d\d): //;
+    my $entry = {
+        date => $1,
+        time => $2
+    };
+
+    # parse operation
+    # Success:  [op]: [params]
+    # Error:    [op](Error: [error]): [params]
+    # Skip:     [op](Skipped): [params]
+    # Startup:  Program start ([user]) [encoding]
+    # Shutdown: Program shutdown ([user])
+
+    # File system plugin [op] starts with "FS:"
+    if (s/^FS://) {
+        $entry->{isFsPluginOp} = 1;
+    }
+
+    if (s/^(\w+): //) {
+        $entry->{operation} = $1;
+        $entry->{success} = 1;
+    } elsif (s/^(\w+)\(Error: (.+)\): //) {
+        $entry->{operation} = $1;
+        $entry->{error} = $2;
+    } elsif (s/^(\w+)\(Skipped\): //) {
+        $entry->{operation} = $1;
+        $entry->{skipped} = 1;
+    } elsif (/^Program start \((.+)\)$/) {
+        $entry->{operation} = "Startup";
+        $entry->{user} = $1;
+    } elsif (/^Program start \((.+)\) (.+)$/) {
+        $entry->{operation} = "Startup";
+        $entry->{user} = $1;
+        $entry->{encoding} = $2;
+    } elsif (/^Program shutdown \((.+)\)$/) {
+        $entry->{operation} = "Shutdown";
+        $entry->{user} = $1;
+    } else {
+        return;
+    }
+
+    # parse params
+    if (s/ -> ((.+)\\(.+))//) {
+        $entry->{destination} = $1;
+        $entry->{destdir} = $2;
+        $entry->{destfile} = $3;
+    }
+    if (/((.+)\\(.+))/) {
+        $entry->{source} = $1;
+        $entry->{sourcedir} = $2;
+        $entry->{sourcefile} = $3;
+    } else {
+        $entry->{source} = $_;
+    }
+
+    return bless $entry, 'RJK::TotalCmd::Log::Entry';
+}
+
+sub readUtf8Bom {
+    (s|^(\xEF\xBB\xBF)||)[0] // "";
 }
 
 1;
