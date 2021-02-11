7183636385c11349a247e3ca3ec4b0c81718cc79
RJK-Engineering <rjklinkhamer@gmail.com>
2021-02-11 12:40:49 +0100

    Update Dir.pl

--- test/RJK/Filecheck/Dir.pl
+++ test/RJK/Filecheck/Dir.pl
@@ -6,11 +6,8 @@ use RJK::Filecheck::Dir;
 
 my $path = 'c:\temp\1';
 my $dir = RJK::Filecheck::Dir->new($path);
-$dir->traverseProperties(sub {
-    my ($k, $v) = @_;
-    print "$k = $v\n";
-    return 0;
-});
+
+dd $dir->getProperties();
 
 print $dir->hasProperty('a')||"no", "\n";
 print $dir->hasProperty('b')||"no", "\n";
@@ -24,22 +21,11 @@ $dir->setProperty('propname1', 'val1');
 $dir->setProperty('propname2', 0);
 $dir->setProperty('propname3', "break\nval2");
 
-$dir->traverseProperties(sub {
-    my ($k, $v) = @_;
-    print "$k = $v\n";
-    return 0;
-});
+dd $dir->getProperties();
 
 $dir->setFileProperty('file.ext', 'propname1', 'val1');
 $dir->setFileProperty('file2.ext', 'propname2', 0);
 $dir->setFileProperty('file.ext', 'propname3', "break\nval2");
-
-$dir->traverseFileProperties(sub {
-    my ($f, $k, $v) = @_;
-    $v //= "undef";
-    print "! $f $k = $v\n";
-    return 0;
-});
 
 my $props = $dir->getFileProperties('file.ext');
 use Data::Dump;
