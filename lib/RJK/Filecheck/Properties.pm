211bcbf937ceaabbd33dfbafee4342301a2640b9
RJK-Engineering <rjklinkhamer@gmail.com>
2021-02-11 12:37:29 +0100

    Update Properties.pm

--- lib/RJK/Filecheck/Properties.pm
+++ lib/RJK/Filecheck/Properties.pm
@@ -3,30 +3,20 @@ package RJK::Filecheck::Properties;
 use strict;
 use warnings;
 
-sub new {
-    my $self = bless {}, shift;
-    $self->{props} = shift // {};
-    return $self;
-}
-
-sub properties {
-    $_[0]{props};
-}
-
 sub get {
-    $_[0]{props}{$_[1]} // "";
+    $_[0]{$_[1]} // "";
 }
 
 sub has {
-    defined $_[0]{props}{$_[1]} && $_[0]{props}{$_[1]} ne "";
+    defined $_[0]{$_[1]} && $_[0]{$_[1]} ne "";
 }
 
 sub set {
-    $_[0]{props}{$_[1]} = $_[2];
+    $_[0]{$_[1]} = $_[2];
 }
 
 sub delete {
-    $_[0]{props}{$_[1]} = "";
+    $_[0]{$_[1]} = "";
 }
 
 1;
