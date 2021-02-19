a095e2ee9106bb132150280db24302e315f93882
RJK-Engineering <rjklinkhamer@gmail.com>
2021-02-19 16:37:27 +0100

    Create Dir.pm

---
+++ lib/TBM/Dir.pm
@@ -0,0 +1,29 @@
+package TBM::Dir;
+use parent 'TBM::Object';
+
+use strict;
+use warnings;
+
+sub getFiles {
+    my ($self) = @_;
+    my %files;
+    ::table('TBM::Path')->select({tail_id => $self->{id}}, sub {
+        my $path = shift;
+        $files{$path->{filename}} = $path->head;
+        return 0;
+    });
+    return \%files;
+}
+
+sub addFile {
+    my ($self, $file) = @_;
+   ::table('TBM::Path')->insert({
+        head_id => $file->{id},
+        head_class => $file->{class},
+        tail_id => $self->{id},
+        tail_class => $self->{class},
+        filename => $file->{name},
+    });
+}
+
+1;
