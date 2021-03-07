53ff818511ca5bceebdd6cfcabcf16f7173667c5
RJK-Engineering <rjklinkhamer@gmail.com>
2021-03-07 04:41:16 +0100

    Update Env.pl

--- test/RJK/Env.pl
+++ test/RJK/Env.pl
@@ -7,9 +7,23 @@ print RJK::Env->subst("%TEMP%"), "\n";
 
 my $byRef1 = "1 %TEMP%";
 my $byRef2 = "2 %TEMP%";
+print $byRef1, "\t";
+print $byRef2, "\n";
 RJK::Env->subst(\$byRef1, \$byRef2);
-print $byRef1, "\n";
+print $byRef1, "\t";
 print $byRef2, "\n\n";
 
-my @p = RJK::Env->findLocalFiles("RJK-utils/filecheck.properties");
+my @p = ('C:\asfdsf', 'C:\Program Files');
+print '"', join('", "', @p), "\"\n";
+@p = RJK::Env->findPath(@p);
+print join("\n", @p), "\n\n";
+
+my $p = "RJK-utils/filecheck.properties";
+print $p, "\n";
+@p = RJK::Env->findLocalFiles($p);
+print join("\n", @p), "\n\n";
+
+$p = "Common Files";
+print $p, "\n";
+@p = RJK::Env->findProgramDirs($p);
 print join("\n", @p), "\n";
