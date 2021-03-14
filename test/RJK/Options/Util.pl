149ae8c2806d4bd35fc8667fa6bb9766fb779114
RJK-Engineering <rjklinkhamer@gmail.com>
2021-03-14 15:14:47 +0100

    Update Util.pl

--- test/RJK/Options/Util.pl
+++ test/RJK/Options/Util.pl
@@ -3,8 +3,21 @@ use warnings;
 
 use RJK::Options::Util;
 my $opts = {
-    path => 'c:/temp/1',
     #~ listFile => 'paths.txt',
+    path => 'c:/temp/1',
+    stdin => 1,
     maxDepth => 1,
 };
+
+if ($opts->{listFile}) {
+    print "\$opts->{listFile}: $opts->{listFile}\n";
+} elsif ($opts->{path}) {
+    print "\$opts->{path}: $opts->{path}\n";
+} elsif (@ARGV) {
+    print "\@ARGV: @ARGV\n";
+} elsif ($opts->{stdin}) {
+    print "Standard input\n";
+} else {
+    print "Current working directory\n";
+}
 RJK::Options::Util->traverseFiles($opts, sub {print "$_\n"});
