239c35d707448b8d80bfd5bb40124d1213cbfc01
RJK-Engineering <rjklinkhamer@gmail.com>
2021-03-16 16:00:36 +0100

    Update TraverseStats.pm

--- lib/RJK/File/TraverseStats.pm
+++ lib/RJK/File/TraverseStats.pm
@@ -3,17 +3,7 @@ package RJK::File::TraverseStats;
 use strict;
 use warnings;
 
-my @fields = qw(
-preVisitDir
-postVisitDir
-preVisitFiles
-postVisitFiles
-visitFile
-visitFileFailed
-size
-files
-dirs
-);
+my @fields = qw(size files dirs failed);
 
 sub new {
     return bless { map { $_ => 0 } @fields }, shift;
@@ -21,50 +11,22 @@ sub new {
 
 sub preVisitDir {
     my $self = shift;
-    $self->{preVisitDir}++;
-
-    $self->{files} += @{$_[2]} if $_[2];
-    $self->{dirs} += @{$_[3]} if $_[3];
-
+    $self->{dirs}++;
     $self->{dirStats} = __PACKAGE__->new;
-    $self->{dirStats}{files} = @{$_[2]} if $_[2];
-    $self->{dirStats}{dirs} = @{$_[3]} if $_[3];
-}
-
-sub postVisitDir {
-    my $self = shift;
-    $self->{postVisitDir}++;
-}
-
-sub preVisitFiles {
-    my $self = shift;
-    $self->{preVisitFiles}++;
-}
-
-sub postVisitFiles {
-    my $self = shift;
-    $self->{postVisitFiles}++;
+    $self->{dirStats}{dirs}++;
 }
 
 sub visitFile {
     my ($self, $file, $stat) = @_;
-
-    $self->{visitFile}++;
+    $self->{files}++;
     $self->{size} += $stat->size || 0;
-
-    if ($self->{dirStats}) {
-        $self->{dirStats}->visitFile($file, $stat);
-    }
+    $self->{dirStats}->visitFile($file, $stat) if $self->{dirStats};
 }
 
 sub visitFileFailed {
     my $self = shift;
-
-    $self->{visitFileFailed}++;
-
-    if ($self->{dirStats}) {
-        $self->{dirStats}->visitFileFailed(@_);
-    }
+    $self->{failed}++;
+    $self->{dirStats}->visitFileFailed(@_) if $self->{dirStats};
 }
 
 sub update {
