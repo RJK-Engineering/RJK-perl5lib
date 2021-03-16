3c22a1d7099833958a8980e7bc568ecc1b83db3b
RJK-Engineering <rjklinkhamer@gmail.com>
2021-03-16 16:00:09 +0100

    Update Size.pm

--- lib/RJK/HumanReadable/Size.pm
+++ lib/RJK/HumanReadable/Size.pm
@@ -3,28 +3,29 @@ package RJK::HumanReadable::Size;
 use strict;
 use warnings;
 
-our @symbols = qw(K M G T P E Z Y);
+our @symbols = qw(b K M G T P E Z Y);
 our $precision = 3;
 our ($number, $symbol, $exact);
 
 sub get {
     my $self = shift;
-    $exact = shift || return "";
+    $exact = shift // return "";
 
-    $symbol = "";
-    return $number = $exact if $exact < 1024;
+    if ($exact < 1024) {
+        $number = $exact;
+        $symbol = $symbols[0];
+    } else {
+        my $si = 0;
+        do {
+            $exact /= 1024;
+            $si++;
+            $number = int $exact;
+        } while (length $number > 3);
 
-    my $si = -1;
-    do {
-        $exact /= 1024;
-        $si++;
-        $number = int $exact;
-    } while (length $number > 3);
-
-    my $z = $exact == $number ? ".000000" : "000000";
-    $number = substr($exact.$z, 0, $precision+1) =~ s/\.$//r;
-    $symbol = $symbols[$si] // "X";
-
+        my $z = $exact == $number ? ".000000" : "000000";
+        $number = substr($exact.$z, 0, $precision+1) =~ s/\.$//r;
+        $symbol = $symbols[$si] // "?";
+    }
     return $number.$symbol;
 }
 
