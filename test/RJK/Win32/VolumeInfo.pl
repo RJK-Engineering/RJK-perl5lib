c13a106e62fe26c08b78607d51b58bccdba31a49
RJK-Engineering <rjklinkhamer@gmail.com>
2021-05-18 12:09:08 +0200

    Update VolumeInfo.pl

--- test/RJK/Win32/VolumeInfo.pl
+++ test/RJK/Win32/VolumeInfo.pl
@@ -7,5 +7,8 @@ my $volumes = RJK::Win32::VolumeInfo->getVolumes();
 use Data::Dump;
 dd $volumes;
 
+my @volumes = RJK::Win32::VolumeInfo->getVolumes();
+dd \@volumes;
+
 my ($free, $total, $available) = RJK::Win32::VolumeInfo->getUsage('C:');
 print "$free, $total, $available\n";
