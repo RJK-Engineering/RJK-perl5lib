1e2fb5afd99b3a9b49a660ce1199928557d6daa1
RJK-Engineering <rjklinkhamer@gmail.com>
2021-10-05 01:37:40 +0200

    Update FileMonitor.pm

--- lib/RJK/Util/FileMonitor.pm
+++ lib/RJK/Util/FileMonitor.pm
@@ -9,25 +9,23 @@ use RJK::Stat;
 sub setFile {
     my $self = shift;
     $self->{file} = shift;
-    my $stat = RJK::Stat->get($self->{file});
-    $self->{modified} = $stat->modified;
-    $self->{size} = $stat->size;
+    $self->{stat} = RJK::Stat->get($self->{file});
 }
 
 sub doPoll {
     my $self = shift;
     my $stat = RJK::Stat->get($self->{file});
 
-    if ($stat->modified != $self->{modified} || $stat->size != $self->{size}) {
+    if ($stat->modified != $self->{stat}->modified || $stat->size != $self->{stat}->size) {
         $self->notifyObservers({
-            type => "FileChanged",
+            type => "FileChangeEvent",
             payload => {
                 file => $self->{file},
                 stat => $stat
             }
         });
     }
-    $self->{modified} = $stat->modified;
+    $self->{stat} = $stat;
 }
 
 1;
