acdccb4d3bf1df93c5f8f68ab29507e57357970f
RJK-Engineering <rjklinkhamer@gmail.com>
2021-11-28 08:39:09 +0100

    Update FileVisitResult.pm

--- lib/FileVisitResult.pm
+++ lib/FileVisitResult.pm
@@ -9,23 +9,18 @@ use constant {
     SKIP_SUBTREE => bless([], 'FileVisitResult::SKIP_SUBTREE'),
     SKIP_SIBLINGS => bless([], 'FileVisitResult::SKIP_SIBLINGS'),
     SKIP_DIRS => bless([], 'FileVisitResult::SKIP_DIRS'),
-    SKIP_FILES => bless([], 'FileVisitResult::SKIP_FILES'),
+    SKIP_FILES => bless([], 'FileVisitResult::SKIP_FILES')
 };
 
-@FileVisitResult::CONTINUE::ISA = qw(FileVisitResult);
-@FileVisitResult::TERMINATE::ISA = qw(FileVisitResult);
-@FileVisitResult::SKIP_SUBTREE::ISA = qw(FileVisitResult);
-@FileVisitResult::SKIP_SIBLINGS::ISA = qw(FileVisitResult);
-@FileVisitResult::SKIP_DIRS::ISA = qw(FileVisitResult);
-@FileVisitResult::SKIP_FILES::ISA = qw(FileVisitResult);
+@FileVisitResult::CONTINUE::ISA =
+@FileVisitResult::TERMINATE::ISA =
+@FileVisitResult::SKIP_SUBTREE::ISA =
+@FileVisitResult::SKIP_SIBLINGS::ISA =
+@FileVisitResult::SKIP_DIRS::ISA =
+@FileVisitResult::SKIP_FILES::ISA = __PACKAGE__;
 
-sub matches {
-    shift;
-    UNIVERSAL::isa(my $result = shift, 'FileVisitResult') || return 0;
-    foreach (@_) {
-        return 1 if $result == $_;
-    }
-    return 0;
+sub isaFileVisitResult {
+    UNIVERSAL::isa($_[1], __PACKAGE__);
 }
 
 1;
