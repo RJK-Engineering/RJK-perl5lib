132de914d111b56a3f3456c9ebec2d233406f191
RJK-Engineering <rjklinkhamer@gmail.com>
2022-01-16 16:36:06 +0100

    Update check.bat

--- check.bat
+++ check.bat
@@ -1,5 +1,6 @@
-@echo off
-set PREVD="%CD%"
+@ECHO OFF
+REM SETLOCAL restores %CD% when bat script ends (or on ENDLOCAL)
+SETLOCAL
+
 cd /d "%~dp0"
 perl c:\scripts\check.pl lib lib\Exceptions %*
-cd /d %PREVD%
